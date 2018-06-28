8df20c04f7fc29891ad7f80c4982fc482a7b06b0
emily colon <emilycolonHIG@gmail.com>
2018-06-28 10:17:31 -0400

    updated to include comments re: spotify mdk player

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,47 @@
-// console.log('script.js linked');
-// window.onSpotifyWebPlaybackSDKReady = () => {
-//   const token =
-//     'BQALiJBuZ2adZkyLAlGPvd2eXXX20xwth3sO2CQyGvfq420U2EiCe43bi1gqeHK9I0tq2a4wYwYZ7U8hVozeZrG6WTNh6B7atoA9slWgoVTPAhoaaRWp6a526T9Ql8YJPUVCTmLykFrSu6iDAl02TSVDIpcj6bNO';
-//   const player = new Spotify.Player({
-//     name: 'music.fav',
-//     getOAuthToken: cb => {
-//       cb(token);
-//     }
-//   });
+// leaving this code in for presentation purposes.  The authentication token
+// listed below is only good for 60 minutes.  If loading the project any other
+// time than within 60 mins of this token being generated, errors will appear
+// in the JS Console due to lack of authentication.
 
-//   // Error handling
-//   player.addListener('initialization_error', ({ message }) => {
-//     console.error(message);
-//   });
-//   player.addListener('authentication_error', ({ message }) => {
-//     console.error(message);
-//   });
-//   player.addListener('account_error', ({ message }) => {
-//     console.error(message);
-//   });
-//   player.addListener('playback_error', ({ message }) => {
-//     console.error(message);
-//   });
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token =
+    'BQALiJBuZ2adZkyLAlGPvd2eXXX20xwth3sO2CQyGvfq420U2EiCe43bi1gqeHK9I0tq2a4wYwYZ7U8hVozeZrG6WTNh6B7atoA9slWgoVTPAhoaaRWp6a526T9Ql8YJPUVCTmLykFrSu6iDAl02TSVDIpcj6bNO';
+  const player = new Spotify.Player({
+    name: 'Web Playback SDK Quick Start Player',
+    getOAuthToken: cb => {
+      cb(token);
+    }
+  });
 
-//   // Playback status updates
-//   player.addListener('player_state_changed', state => {
-//     console.log(state);
-//   });
+  // Error handling
+  player.addListener('initialization_error', ({ message }) => {
+    console.error(message);
+  });
+  player.addListener('authentication_error', ({ message }) => {
+    console.error(message);
+  });
+  player.addListener('account_error', ({ message }) => {
+    console.error(message);
+  });
+  player.addListener('playback_error', ({ message }) => {
+    console.error(message);
+  });
 
-//   // Ready
-//   player.addListener('ready', ({ device_id }) => {
-//     console.log('Ready with Device ID', device_id);
-//   });
+  // Playback status updates
+  player.addListener('player_state_changed', state => {
+    console.log(state);
+  });
 
-//   // Not Ready
-//   player.addListener('not_ready', ({ device_id }) => {
-//     console.log('Device ID has gone offline', device_id);
-//   });
+  // Ready
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+  });
 
-//   // Connect to the player!
-//   player.connect();
-// };
+  // Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+  });
+
+  // Connect to the player!
+  player.connect();
+};
